9fe48843598557c6824d35212b7584f28eb0276a
Sunwoo Yeom <o55306605@gmail.com>
2025-04-09 15:14:56 -0400

    removed local package bc it cause error

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
 
 Future<void> _messageHandler(RemoteMessage message) async {
   print('background message ${message.notification!.body}');
